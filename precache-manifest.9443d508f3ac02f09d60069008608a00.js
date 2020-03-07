e470bf165120f88323d439615071546f52fa3226
Carolina Acosta <carolinaacosta.n@gmail.com>
2020-03-07 15:28:48 +0000

    Updates

---
+++ precache-manifest.9443d508f3ac02f09d60069008608a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcb8317297410342f3cd7635572e5f8",
+    "url": "/Tic-tac-toe-React/index.html"
+  },
+  {
+    "revision": "1b642ae511f26fa8a40e",
+    "url": "/Tic-tac-toe-React/static/css/main.b6002f3a.chunk.css"
+  },
+  {
+    "revision": "51f46ae0e90f04920919",
+    "url": "/Tic-tac-toe-React/static/js/2.1f80523c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tic-tac-toe-React/static/js/2.1f80523c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b642ae511f26fa8a40e",
+    "url": "/Tic-tac-toe-React/static/js/main.55afa9e0.chunk.js"
+  },
+  {
+    "revision": "95336c2cf73eccb89b67",
+    "url": "/Tic-tac-toe-React/static/js/runtime-main.fcf10fd3.js"
+  }
+]);
